813e18051c9bf296d661ca76b8e3f8aec7bda79a
Aditya Ranjan <adityaranjan25@gmail.com>
2021-08-26 17:02:16 +0530

    Create theme.js

---
+++ theme.js
@@ -0,0 +1,5 @@
+function changeLogo() {
+    document.getElementsByClassName("courseIconBox")[0].getElementsByTagName("img")[0].src = "https://i.ibb.co/yhhj4xz/lab-logo.png";
+}
+
+setTimeout(changeLogo, 1);
